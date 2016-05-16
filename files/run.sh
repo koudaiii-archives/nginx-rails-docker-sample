d4d639321d194f399b32c9da41cabedd84df49d9
koudaiii <cs006061@gmail.com>
2016-05-16 18:01:37 +0900

    Change LB

--- files/run.sh
+++ files/run.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
-blue_url=${BLUE_URL}
-blue_port=${BLUE_PORT}
-green_url=${GREEN_URL}
-green_port=${GREEN_PORT}
+app_url=${APP_URL}
+app_port=${APP_PORT}
 
-sed -i "s/%BLUE_URL%/$blue_url/g" /etc/nginx/conf.d/nginx-docker.conf
-sed -i "s/%BLUE_PORT%/$blue_port/g" /etc/nginx/conf.d/nginx-docker.conf
-sed -i "s/%GREEN_URL%/$green_url/g" /etc/nginx/conf.d/nginx-docker.conf
-sed -i "s/%GREEN_PORT%/$green_port/g" /etc/nginx/conf.d/nginx-docker.conf
+sed -i "s/%APP_URL%/$app_url/g" /etc/nginx/conf.d/nginx-docker.conf
+sed -i "s/%APP_PORT%/$app_port/g" /etc/nginx/conf.d/nginx-docker.conf
 
 nginx -g "daemon off;"
